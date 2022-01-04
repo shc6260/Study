50ad2624a06f54cd76e6687410f615ec55c76348
shc6260 <shc6260@gmail.com>
2022-01-05 00:00:24 +0900

    mvvm

---
+++ C# 3-2/0928/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btntry = new System.Windows.Forms.Button();
+            this.nfiTray = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cmsMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.폼보이기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.종료ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmsMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btntry
+            // 
+            this.btntry.Location = new System.Drawing.Point(40, 24);
+            this.btntry.Name = "btntry";
+            this.btntry.Size = new System.Drawing.Size(136, 36);
+            this.btntry.TabIndex = 0;
+            this.btntry.Text = "트레이 이동";
+            this.btntry.UseVisualStyleBackColor = true;
+            this.btntry.Click += new System.EventHandler(this.btntry_Click);
+            // 
+            // nfiTray
+            // 
+            this.nfiTray.ContextMenuStrip = this.cmsMenu;
+            this.nfiTray.Icon = ((System.Drawing.Icon)(resources.GetObject("nfiTray.Icon")));
+            this.nfiTray.Text = "트레이아이콘";
+            this.nfiTray.Visible = true;
+            this.nfiTray.DoubleClick += new System.EventHandler(this.nfiTray_DoubleClick);
+            // 
+            // cmsMenu
+            // 
+            this.cmsMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.폼보이기ToolStripMenuItem,
+            this.종료ToolStripMenuItem});
+            this.cmsMenu.Name = "cmsMenu";
+            this.cmsMenu.Size = new System.Drawing.Size(123, 48);
+            // 
+            // 폼보이기ToolStripMenuItem
+            // 
+            this.폼보이기ToolStripMenuItem.Name = "폼보이기ToolStripMenuItem";
+            this.폼보이기ToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.폼보이기ToolStripMenuItem.Text = "폼보이기";
+            this.폼보이기ToolStripMenuItem.Click += new System.EventHandler(this.폼보이기ToolStripMenuItem_Click);
+            // 
+            // 종료ToolStripMenuItem
+            // 
+            this.종료ToolStripMenuItem.Name = "종료ToolStripMenuItem";
+            this.종료ToolStripMenuItem.Size = new System.Drawing.Size(122, 22);
+            this.종료ToolStripMenuItem.Text = "종료";
+            this.종료ToolStripMenuItem.Click += new System.EventHandler(this.종료ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 82);
+            this.Controls.Add(this.btntry);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "트레이 아이콘";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.cmsMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btntry;
+        private System.Windows.Forms.NotifyIcon nfiTray;
+        private System.Windows.Forms.ContextMenuStrip cmsMenu;
+        private System.Windows.Forms.ToolStripMenuItem 폼보이기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 종료ToolStripMenuItem;
+    }
+}
+
---
+++ WpfApp6/WpfApp6/ViewModel/MainViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfApp6.Model;
+
+namespace WpfApp6.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private MainModel myModel = null;
+
+        public MainViewModel()
+        {
+            myModel = new MainModel();
+            string n = Dollar;
+            MessageBox.Show(n);
+            
+        }
+        public string Dollar
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(myModel.dollar))
+                {
+                    Won = "0";
+                }
+                else
+                {
+                    int num = -1;
+                    if (int.TryParse(myModel.dollar, out num))
+                    {
+                        int result = num * 1160;
+                        Won = result.ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please insert number", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        Dollar = "";
+                        Won = "0";
+                    }
+                }
+                return myModel.dollar;
+            }
+            set
+            {
+                if (myModel.dollar != value)
+                {
+                    myModel.dollar = value;
+                    OnPropertyChanged("Dollar");
+                }
+            }
+        }
+
+        public string Won
+        {
+            get
+            {
+                return myModel.won;
+            }
+            set
+            {
+                myModel.won = value;
+                OnPropertyChanged("Won");
+            }
+        }
+        
+       
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string prop)
+        { 
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
